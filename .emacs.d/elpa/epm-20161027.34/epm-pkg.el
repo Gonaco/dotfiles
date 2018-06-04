9d621f951c563c7761b04b547263765c33cb86d7
Gonaco <danielmoremanza@gmail.com>
2018-06-04 23:25:16 +0200

    Emacs dotfiles from old Emacs dot files from koiserver

---
+++ .emacs.d/elpa/epm-20161027.34/epm-pkg.el
@@ -0,0 +1,7 @@
+(define-package "epm" "20161027.34" "Emacs Package Manager"
+  '((emacs "24.3")
+    (epl "0.8"))
+  :url "https://github.com/xuchunyang/epm")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/log4e-20150105.505/log4e-autoloads.el
@@ -0,0 +1,16 @@
+;;; log4e-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("log4e-pkg.el" "log4e.el") (22680 23087
+;;;;;;  850519 995000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; log4e-autoloads.el ends here
